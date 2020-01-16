ff0b30d06614fab51ac1250b96d9df44d9a05d5f
bethebikash <bhattaraibikash55@gmail.com>
2020-01-16 16:46:30 +0545

    Image Slider added in homepage of the application.

--- app/src/main/java/com/bhattaraibikash/erepair/fragment/main/HomeFragment.java
+++ app/src/main/java/com/bhattaraibikash/erepair/fragment/main/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.bhattaraibikash.erepair.fragment.main;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -15,11 +16,18 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.bhattaraibikash.erepair.R;
+import com.bhattaraibikash.erepair.adapter.SliderAdapter;
+import com.smarteist.autoimageslider.IndicatorAnimations;
+import com.smarteist.autoimageslider.IndicatorView.draw.controller.DrawController;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+
+    private SliderView sliderView;
 
 
     public HomeFragment() {
@@ -32,7 +40,31 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // Slider
+        sliderView = view.findViewById(R.id.imageSlider);
+
+        final SliderAdapter adapter = new SliderAdapter(getContext());
+        adapter.setCount(5);
+
+        sliderView.setSliderAdapter(adapter);
+
+        sliderView.setIndicatorAnimation(IndicatorAnimations.SLIDE); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+        sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_RIGHT);
+        sliderView.setIndicatorSelectedColor(Color.WHITE);
+        sliderView.setIndicatorUnselectedColor(Color.GRAY);
+        sliderView.startAutoCycle();
+
+        sliderView.setOnIndicatorClickListener(new DrawController.ClickListener() {
+            @Override
+            public void onIndicatorClicked(int position) {
+                sliderView.setCurrentPagePosition(position);
+            }
+        });
+
+        return (view);
     }
 
 
