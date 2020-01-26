1b84073b76af0341cae994f196c5185f7c6a5251
bethebikash <bhattaraibikash55@gmail.com>
2020-01-26 22:49:10 +0545

    review recycler view added in service detail page.

--- app/src/main/java/com/bhattaraibikash/erepair/activities/service/ServiceDetailActivity.java
+++ app/src/main/java/com/bhattaraibikash/erepair/activities/service/ServiceDetailActivity.java
@@ -1,12 +1,25 @@
 package com.bhattaraibikash.erepair.activities.service;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bhattaraibikash.erepair.R;
+import com.bhattaraibikash.erepair.adapter.ReviewAdapter;
+import com.bhattaraibikash.erepair.models.ReviewResponse;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServiceDetailActivity extends AppCompatActivity {
+
+    private TextView tvService, tvCategorySD, tvPrice;
+    private Button btnBookNow;
+    private RecyclerView rvReview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +28,20 @@ public class ServiceDetailActivity extends AppCompatActivity {
 
         getSupportActionBar().setTitle("Service Detail");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Service Recycler view
+        rvReview = findViewById(R.id.rvReview);
+
+        List<ReviewResponse> reviewResponseList =new ArrayList<>();
+        reviewResponseList.add(new ReviewResponse("0222520", "User one", "address of one", "2514", "This is the review from the customer one."));
+        reviewResponseList.add(new ReviewResponse("5454851", "User two", "address of two", "2514", "This is the review from the customer one."));
+        reviewResponseList.add(new ReviewResponse("2125648", "User three", "address of three", "2514", "This is the review from the customer one."));
+        reviewResponseList.add(new ReviewResponse("2125648", "User four", "address of four", "2514", "This is the review from the customer one."));
+        reviewResponseList.add(new ReviewResponse("2125648", "User five", "address of five", "2514", "This is the review from the customer one."));
+
+        ReviewAdapter reviewAdapter = new ReviewAdapter(this, reviewResponseList);
+        rvReview.setAdapter(reviewAdapter);
+        rvReview.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
     }
 
     @Override
