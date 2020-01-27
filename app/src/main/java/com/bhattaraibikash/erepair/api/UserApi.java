a4a63ac9ebd731e85e7cb7618509ed7eff19632f
bethebikash <bhattaraibikash55@gmail.com>
2020-01-28 02:24:13 +0545

    Login completed.

--- app/src/main/java/com/bhattaraibikash/erepair/api/UserApi.java
+++ app/src/main/java/com/bhattaraibikash/erepair/api/UserApi.java
@@ -1,20 +1,23 @@
 package com.bhattaraibikash.erepair.api;
 
 import com.bhattaraibikash.erepair.models.User;
+import com.bhattaraibikash.erepair.responses.LoginResponse;
 import com.bhattaraibikash.erepair.responses.UserResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 public interface UserApi {
     @POST("users")
     Call<UserResponse> userRegister(@Body User user);
 
-//    @FormUrlEncoded
-//    @POST("/users/login")
-//    Call<SignUpResponse> userLogin(@Field("username") String username, @Field("password") String password);
-//
+    @FormUrlEncoded
+    @POST("/users/login")
+    Call<LoginResponse> userLogin(@Field("username") String username, @Field("password") String password);
+
 //    @Multipart
 //    @POST("upload")
 //    Call<ImageResponse> uploadImage(@Part MultipartBody.Part img);
---
+++ app/src/main/java/com/bhattaraibikash/erepair/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.bhattaraibikash.erepair.controller;
+
+import com.bhattaraibikash.erepair.api.UserApi;
+import com.bhattaraibikash.erepair.responses.LoginResponse;
+import com.bhattaraibikash.erepair.url.Url;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Response;
+
+public class UserController {
+
+    boolean isSuccess = false;
+
+    public boolean checkUser(String username, String password) {
+
+        UserApi userApi = Url.getInstance().create(UserApi.class);
+        Call<LoginResponse> usersCall = userApi.userLogin(username, password);
+
+        try {
+            Response<LoginResponse> loginResponse = usersCall.execute();
+            if (loginResponse.isSuccessful() &&
+                    loginResponse.body().getStatus().equals("200")) {
+
+                Url.token = "Bearer "+loginResponse.body().getToken();
+                isSuccess = true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return isSuccess;
+    }
+}
